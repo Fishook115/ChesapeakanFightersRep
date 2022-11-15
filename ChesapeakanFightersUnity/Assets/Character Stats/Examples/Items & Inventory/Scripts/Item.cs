40591cd7b163a4b437a54381ffa81507173687ed
CharlotteLewter <shashalewter@gmail.com>
2022-11-15 15:48:04 -0500

    Warrior Animations

--- ChesapeakanFightersUnity/Assets/Character Stats/Examples/Items & Inventory/Scripts/Item.cs
+++ ChesapeakanFightersUnity/Assets/Character Stats/Examples/Items & Inventory/Scripts/Item.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 
-namespace Kryz.CharacterStats.Examples
-{
-	[CreateAssetMenu]
+//namespace Kryz.CharacterStats.Examples
+//{
+	//[CreateAssetMenu]
 	public class Item : ScriptableObject
 	{
 		public string ItemName;
 		public Sprite Icon;
 	}
-}
+//}
